90c9c36202047dea551b851bbcfa5f2d6ecf1f91
Aashish Singh <aashishs4912345@gmail.com>
2023-08-18 03:29:25 +0530

    cleaned up eventsjs code

--- js/eventsJS.js
+++ js/eventsJS.js
@@ -1,20 +1,4 @@
-$(document).ready(function() {
-    // Event details toggle
-    $(".show-details").on("click", function() {
-        const eventDetails = $(this).siblings(".event-details");
-        const isActive = eventDetails.hasClass("active");
-
-        if (!isActive) {
-            eventDetails.slideDown();
-            eventDetails.addClass("active");
-        } else {
-            eventDetails.slideUp();
-            eventDetails.removeClass("active");
-        }
-    });
-});
-
-// events js 
 function redirectToGoogleForms() {
     window.open('https://forms.google.com', '_blank');
-}
+  }
+  
